77916f9ec9a0d7e8f863900e13eafbb27f34f833
YassG4mer <73476412+yassinekrika@users.noreply.github.com>
2023-03-15 22:24:15 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -10,7 +10,7 @@ import numpy as np
 class App(customtkinter.CTk):
     def __init__(self) -> None:
         super().__init__()
-        self.title("Huffman Coding")
+        self.title("Tp -3")
         self.geometry("800x500")
         self.maxsize(800, 500)
         self.minsize(800, 500)
@@ -18,38 +18,32 @@ class App(customtkinter.CTk):
 
         font1 = ("Arial", 20)
 
-        self.image_path = ''
-        
-        self.code_button = customtkinter.CTkButton(self, text="select file",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.select_file)
-        self.code_button.place(x=20, y=20)
+        self.image_path = self.image_path = filedialog.askopenfilename(title='Open a file', initialdir='/home/yassg4mer/Project/tp_tmn/echentillonnage/')
+        self.dct_img = None
 
-        self.code_button = customtkinter.CTkButton(self, text="show image",font=font1, fg_color="#FF0000", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.show_img)
-        self.code_button.place(x=300, y=20)
+        self.label = customtkinter.CTkLabel(self, text="selected image : ", width=120, height=25, fg_color=("#17043d"), font=font1, text_color="#ffffff")
+        self.label.place(x=5, y=5)
 
-        self.code_button = customtkinter.CTkButton(self, text="image to YCrCb",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.image_to_ycrcb)
-        self.code_button.place(x=20, y=60)
+        self.path = customtkinter.CTkLabel(self, text=self.image_path, width=120, height=25, fg_color=("#17043d"),  text_color="#ffffff")
+        self.path.place(x=5, y=30)
 
-        self.code_button = customtkinter.CTkButton(self, text="YCrCb to image",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.ycrcb_to_rgb)
-        self.code_button.place(x=20, y=100)
+        self.code_button = customtkinter.CTkButton(self, text="image & YCrCb",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.image_and_ycrcb)
+        self.code_button.place(x=20, y=80)
 
-        self.code_button = customtkinter.CTkButton(self, text="4:2:2",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.sous_ech422)
-        self.code_button.place(x=20, y=140)
-
-        self.code_button = customtkinter.CTkButton(self, text="4:1:1",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.sous_ech411)
-        self.code_button.place(x=20, y=180)
+        self.code_button = customtkinter.CTkButton(self, text="sous echentillonnage",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.sous_ech411)
+        self.code_button.place(x=20, y=160)
 
         self.code_button = customtkinter.CTkButton(self, text="DCT Formula",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.dct_formula)
-        self.code_button.place(x=20, y=220)
-
-        self.code_button = customtkinter.CTkButton(self, text="Inverse DCT Formula",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.inverse_dct_formula)
         self.code_button.place(x=20, y=260)
 
-        self.code_button = customtkinter.CTkButton(self, text="Inverse DCT Matrix",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.dct_matrix)
+        self.code_button = customtkinter.CTkButton(self, text="Inverse DCT Formula",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.inverse_dct_formula)
         self.code_button.place(x=20, y=300)
 
-    def select_file(self):
-            self.image_path = filedialog.askopenfilename(title='Open a file', initialdir='/home/yassg4mer/Project/tp_tmn/echentillonnage/')
-            print(self.image_path)
+        self.code_button = customtkinter.CTkButton(self, text="Inverse DCT Matrix",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.dct_matrix)
+        self.code_button.place(x=20, y=340)
+
+        self.code_button = customtkinter.CTkButton(self, text="psnr",font=font1, fg_color="#03a819", hover_color="#03a819", width=120, corner_radius=20, text_color="#ffffff", command=self.call_psnr)
+        self.code_button.place(x=20, y=380)
 
     def image_to_ycrcb(self):
         
@@ -62,27 +56,11 @@ class App(customtkinter.CTk):
         Cr = 128 + (0.5 * R - 0.419 * G - 0.081 * B)
 
         ycrcb_image = cv2.merge((Y, Cr, Cb))
-        
 
-        cv2.imwrite("/home/yassg4mer/Project/tp_tmn/echentillonnage/YcrCb.bmp", ycrcb_image)
-
-        # show figure
-        plt.figure(figsize=(10,10))
-        plt.subplot(131)
-        plt.imshow(Y)
-        plt.title('Y')
-        plt.subplot(132)
-        plt.imshow(Cr)
-        plt.title('Cr')
-        plt.subplot(133)
-        plt.imshow(Cb)
-        plt.title('Cb')
-        plt.show()
-
+        cv2.imwrite("/home/yassg4mer/Project/tp_tmn/echentillonnage/Ycrcb.bmp", ycrcb_image)
         return ycrcb_image
         
-    def ycrcb_to_rgb(self):
-        ycrcb_image = self.image_to_ycrcb()
+    def ycrcb_to_rgb(self, ycrcb_image):
         Y, Cr, Cb = cv2.split(ycrcb_image)
 
         # Calculate the R, G, and B channels
@@ -94,6 +72,35 @@ class App(customtkinter.CTk):
         rgb_image = cv2.merge((R, G, B))
 
         cv2.imwrite("/home/yassg4mer/Project/tp_tmn/echentillonnage/RGB.bmp", rgb_image)
+
+        return rgb_image
+
+    def image_and_ycrcb(self):
+        ycrcb = self.image_to_ycrcb()
+        rgb = self.ycrcb_to_rgb(ycrcb)
+        
+        y, cr, cb = cv2.split(ycrcb)
+
+        # show figure ycrcb
+        plt.figure(figsize=(10,10))
+        plt.subplot(131)
+        plt.imshow(y)
+        plt.title('Y')
+        plt.subplot(132)
+        plt.imshow(cr)
+        plt.title('Cr')
+        plt.subplot(133)
+        plt.imshow(cb)
+        plt.title('Cb')
+        plt.show()
+
+        # show figure rgb
+        rgb_img = cv2.imread("/home/yassg4mer/Project/tp_tmn/echentillonnage/RGB.bmp")
+        plt.figure(figsize=(10,10))
+        plt.subplot()
+        plt.imshow(rgb_img)
+        plt.title('original img')
+        plt.show()
 
     def sous_ech422(self):
         ycrcb = self.image_to_ycrcb()
@@ -113,8 +120,9 @@ class App(customtkinter.CTk):
         img422 = cv2.merge((y, new_cr_422, new_cb_422))
         
         cv2.imwrite("/home/yassg4mer/Project/tp_tmn/echentillonnage/SEch422.bmp", img422)
-        
 
+
+        
     def sous_ech411(self):
         ycrcb = self.image_to_ycrcb()
         
@@ -133,118 +141,95 @@ class App(customtkinter.CTk):
 
         img411 = cv2.merge((y, new_cr_411, new_cb_411))
         cv2.imwrite("/home/yassg4mer/Project/tp_tmn/echentillonnage/SEch411.bmp", img411)
+    
+    def sous_ech(self):
+        self.sous_ech422()
+        self.sous_ech411()
+
+    def psnr(img1, img2):
+        mse = np.mean((img1 - img2) ** 2)
+        if mse == 0:
+            return float('inf')
+        else:
+            max_pixel = 255.0
+            psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
+
+        print('peack signal noise ration (PSNR) :', psnr)
+
+    def call_psnr(self):
+        img1 = filedialog.askopenfilename(title='img1', initialdir='/home/yassg4mer/Project/tp_tmn/echentillonnage/')
+        img2 = filedialog.askopenfilename(title='img2', initialdir='/home/yassg4mer/Project/tp_tmn/echentillonnage/')
+
+        self.psnr(img1, img2)
 
     # dct
-    def matrix_to_blocks(self, matrix):
-        blocks = []
-        for i in range(0, matrix.shape[0], 8):
-            for j in range(0, matrix.shape[1], 8):
-                block = matrix[i:i+8, j:j+8]
-                blocks.append(block)
-        return blocks
+    def dct(self, img):
+        M, N = img.shape
+        dct_img = np.zeros((M, N))
+
+        # Compute constants c(u) and c(v)
+        c = np.ones((8, 8))
+        c[0, :] = 1 / np.sqrt(2)
+        c[:, 0] = 1 / np.sqrt(2)
+
+        # Compute 2D DCT
+        for u in range(8, M + 1, 8):
+            for v in range(8, N + 1, 8):
+                img_block = img[u-8:u, v-8:v]
+                dct_block = np.zeros((8, 8))
+                for i in range(8):
+                    for j in range(8):
+                        sum_dct = 0.0
+                        for x in range(8):
+                            for y in range(8):
+                                sum_dct += img_block[x, y] * np.cos((2 * x + 1) * i * np.pi / 8) * np.cos((2 * y + 1) * j * np.pi / 8)
+                        dct_block[i, j] = 0.25 * c[i, j] * sum_dct
+                dct_img[u-8:u, v-8:v] = dct_block
+
+        return dct_img
     
-    def c(k):
-        if (k == 0):
-            return np.sqrt(1/8) 
-        else:
-            return np.sqrt(2/8)
+    def inverse_dct(self, dct_img):
+        M, N = dct_img.shape
+        img = np.zeros((M, N))
 
-    def dct_block(self, image):
-        M, N = image.shape
+        # Compute constants c(u) and c(v)
+        c = np.ones((8, 8))
+        c[0, :] = 1 / np.sqrt(2)
+        c[:, 0] = 1 / np.sqrt(2)
 
-        dct_image = np.zeros((M, N))
-        
-        for u in range(M):
-            for v in range(N):
-                sum = 0
-                for x in range(M):
-                    for y in range(N):
-                        cos_x = np.cos((2*x + 1) * u * np.pi / (2*M))
-                        cos_y = np.cos((2*y + 1) * v * np.pi / (2*N))
+        # Compute inverse 2D DCT
+        for x in range(8, M + 1, 8):
+            for y in range(8, N + 1, 8):
+                dct_block = dct_img[x-8:x, y-8:y]
+                img_block = np.zeros((8, 8))
+                for i in range(8):
+                    for j in range(8):
+                        sum_idct = 0.0
+                        for u in range(8):
+                            for v in range(8):
+                                sum_idct += c[u, v] * dct_block[u, v] * np.cos((2 * i + 1) * u * np.pi / 16) * np.cos((2 * j + 1) * v * np.pi / 16)
+                        img_block[i, j] = 0.25 * sum_idct
+                img[x-8:x, y-8:y] = img_block
 
-                        sum += image[x,y] * cos_x * cos_y
-
-
-                dct_image[u,v] = self.c(u) * self.c(v) * sum
-        
-        return dct_image
+        return img
     
-
-    def inverse_dct_block(self, dct_image):
-        M, N = dct_image.shape
-        image = np.zeros((M, N))
-        
-        for x in range(M):
-            for y in range(N):
-                sum = 0
-                for u in range(M):
-                    for v in range(N):
-                        cos_x = np.cos((2*x + 1) * u * np.pi / (2*M))
-                        cos_y = np.cos((2*y + 1) * v * np.pi / (2*N))
-
-                        sum += self.c(u) * self.c(v) * dct_image[u,v] * cos_x * cos_y
-
-
-                image[x,y] = sum
-        
-        return image
-
     def dct_formula(self):
-        blocks = self.matrix_to_blocks(self.image_path)
-        dct = []
-        for block in blocks:
-            dct.append(self.dct_block(block))
+        img = cv2.imread('original_image.bmp', cv2.IMREAD_GRAYSCALE)
+        self.dct_img = self.dct(img)
+        cv2.imshow('Original Image', img)
+        cv2.imshow('DCT Image', self.dct_img.astype('uint8'))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def inverse_dct_formula(self):
-        pass
-
+        img_reconstructed = self.idct_2d(self.dct_img)
+        cv2.imshow('Inverse dct image', img_reconstructed.astype('uint8'))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     # dct matrix
     def dct_matrix(matrix):
-        m = 8
-        n = 8
-
-        def c(k):
-            if (k == 0):
-                return 1 / (np.sqrt(m))
-            else:
-                return np.sqrt(2 / m)
-            
-        dct = np.zeros(m, n)
-    
-        for i in range(m):
-            for j in range(n):
-
-                if (i == 0 and j == 0):
-                    ci = c(i)
-                    cj = c(j)
-                else:
-                    ci = c(i)
-                    cj = c(j)
-
-
-                sum = 0
-                for k in range(m):
-                    for l in range(n):
-    
-                        sum += matrix[k][l] * np.cos((2 * k + 1) * i * np.pi / (
-                            2 * m)) * np.cos((2 * l + 1) * j * np.pi / (2 * n))
-
-    
-                dct[i][j] = ci * cj * sum
-
-        # test
-        for i in range(m):
-            for j in range(n):
-                print(dct[i][j], end="\t")
-            print()
- 
-    # show
-    def show_img(self):
-        img = cv2.imread("original_image.bmp", cv2.IMREAD_ANYCOLOR)
-        plt.subplot(133)
-        plt.imshow(img)
-        plt.show()
+        pass
 
 if __name__=="__main__":
     app = App()
